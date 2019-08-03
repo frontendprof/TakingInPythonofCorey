0c052ccdf6f90cf8ecdbdf2054d1da514678d7d5
Abdumalik Sharipov <abdumaliksharipov@gmail.com>
2019-08-03 02:33:38 -0500

    Some css and bootstrap added to our flask blogpost

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -1,18 +1,44 @@
+# B_R_R
+# M_S_A_E
 
 
-
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+
+posts=[
+	{
+		'author': "AbdlMalik Sharif",
+		'title': 'My first Blog Post',
+		'content': 'First post content',
+		'date_posted': 'August 3, 2019'
+	},
+	{
+		'author': "Corey Schafer",
+		'title': 'Full-featured web app on flask',
+		'content': 'Technical post content',
+		'date_posted': 'April 21, 2018'
+	},
+	{
+		'author': "Engineer Man",
+		'title': 'Why to switch to Linux',
+		'content': 'Switching to Linux and its content',
+		'date_posted': 'January 12, 2019'
+	}
+
+
+
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h2>Home Page</h2>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h2>About Page</h2>'
-
+    return render_template('about.html', title='About')
 if __name__=='__main__':
 	app.run(debug=True)
