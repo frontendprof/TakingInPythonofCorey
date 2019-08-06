360d3e6a995ba29579e12ef343b47f32f7e58c66
Abdumalik Sharipov <abdumaliksharipov@gmail.com>
2019-08-06 18:17:15 -0500

    User Authentication feature made available

--- Flask_blog/flaskblog/__init__.py
+++ Flask_blog/flaskblog/__init__.py
@@ -1,6 +1,8 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 
@@ -8,6 +10,10 @@ app = Flask(__name__)
 app.config["SECRET_KEY"]="bfcde87158a097d9f35eb296443340f1"
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db=SQLAlchemy(app)
+b_c=Bcrypt(app)
+l_manager=LoginManager(app)
+l_manager.login_view='login'
+l_manager.login_message_category='info'
 
 
 from flaskblog import routes
